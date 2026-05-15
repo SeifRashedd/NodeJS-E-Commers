6b700f50166b4bc85ab6255621446fac7364a232
Seif Rashed <seifrashedswe@gmail.com>
2026-05-15 12:22:31 +0300

    add morgan middleware for logging and update dependencies in package.json and package-lock.json

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
 dotenv.config({ path: 'config.env' });
 
 const app = express();
+
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+    console.log(`${process.env.NODE_ENV} mode`);
+}
+
+
 
 
 app.get('/', (req, res) => {
